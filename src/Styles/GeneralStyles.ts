e257aa4d3c57ac50c7f71ec9972f1252ca3df325
Emma Kim <emmakim@crc-dot1x-nat-10-239-70-131.bu.edu>
2024-04-26 03:42:47 -0400

    button

--- src/Styles/GeneralStyles.ts
+++ src/Styles/GeneralStyles.ts
@@ -12,92 +12,113 @@ export const GlobalStyle = createGlobalStyle`
 `;
 
 
-export const StyledInput = styled.input`
-    padding: 5px;
-    //margin-left: 10px;
-    //margin-right: 20px;
-    border-radius: 5px;
-    border-color: #c4c4c4;
-    border-width: thin;
-    text-shadow: black;
-    display: flex;
-    margin: auto;
-    margin-bottom: 20px;
 
+
+export const StyledInput = styled.input`
+  padding: 5px;
+  //margin-left: 10px;
+  //margin-right: 20px;
+  border-radius: 5px;
+  border-color: #c4c4c4;
+  border-width: thin;
+  text-shadow: black;
+  display: flex;
+  margin: auto;
+  margin-bottom: 20px;
 `
+export const StyledCustomLink = styled.a`
+  font-family: 'Satoshi', sans-serif;
+  color: #4e3280;
+  text-decoration: none;
+  cursor: pointer;
+`;
 
 export const StyledH1 = styled.h1`
-    align-items: center;
-    font-family: 'Satoshi', sans-serif;
-    font-weight: bold;
-    padding-right: 10px;
-    padding-left: 10px;
+  align-items: center;
+  font-family: 'Satoshi', sans-serif;
+  font-weight: 900;
+  padding-right: 10px;
+  padding-left: 10px;
 
 `
 
 export const LoginDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: auto;
-    align-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-content: center;
+  align-items: center;
 `
 
 export const StyledButton = styled.button`
-    padding: 5px 20px 5px 20px;
-    border-radius: 4px;
-    border-width: thin;
-    background-color: black;
-    color: white;
-    font-family: 'Satoshi', sans-serif;
-    font-weight: bold;
-    box-shadow: black;
-    display: flex;
-    margin: auto;
+  padding: 10px 45px;
+  border-radius: 31px;
+  //border-width: thin;
+  background: linear-gradient(to right, #5B4EF0, #67B6FF);
+  color: white;
+  font-family: 'Satoshi', sans-serif;
+  font-weight: normal;
+  //box-shadow: black;pm
+  display: flex;
+  margin: auto;
+  font-size: 15pt;
+  border: none;
+  margin-left: 5px;
+  margin-right: 5px;
+  border: none;
 `
 
 export const InputDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 5px;
-    align-content: center;
-    padding-left: 10px;
-    padding-right: 10px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 5px;
+  align-content: center;
+  padding-left: 10px;
+  padding-right: 10px;
 `
 
 export const StyledLabel = styled.label`
-    font-family: 'Satoshi', sans-serif;
-    display: flex;
-    margin: auto;
+  font-family: 'Satoshi', sans-serif;
+  display: flex;
+  margin: auto;
 `
 
-export const LandingDiv = styled.div`  
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    
-    height: 100vh;
+export const LandingDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `
 
 export const StyledContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    
-    padding: 20px 30px 50px;
-    
-    background: linear-gradient(0deg, #d0c6ff, #ffccd5);
-    border: 3px solid #ffffff;
-    border-radius: 20px;
-    
-    box-shadow: 0 0 15px rgba(0, 0, 0, 0.31);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 30px 50px;
+  background: linear-gradient(0deg, #fafafa, #ffffff);
+  border: 3px solid #ffffff;
+  border-radius: 20px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.14);
 `
 
 export const ErrorMessage = styled.span`
-    color: red;
-    text-transform: uppercase;
+  color: #942c2c;
+  text-transform: uppercase;
+`;
+
+// export const StyledLink = styled(Link)`
+//     padding-top: 20px;
+//     font-family: 'Satoshi', sans-serif;
+//     color: #4e3280;
+//     //font-weight: lighter;
+// `;
+export const StyledP=styled.p`
+  padding-top: 10px;
+  font-family: 'Satoshi', sans-serif;
+  color: #4e3280;
+  font-weight: lighter;
 `;
 
 export const StyledLink = styled(Link)`
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
-import {LandingDiv, StyledH1, StyledLink, StyledContainer} from "@/Styles/GeneralStyles";
+"use client"
+import Link from "next/link";
+import styled from "styled-components";
+import {LandingDiv, StyledH1, StyledLink, StyledContainer, StyledButton, StyledCustomLink} from "@/Styles/GeneralStyles";
+
+const StyledButtonContainer=styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+
 export default function Home() {
+    return (
+        <>
+            <LandingDiv>
+                <StyledContainer>
+                    <StyledH1> Personality Quiz! </StyledH1>
 
-
-  return (
-    <LandingDiv>
-        <StyledContainer>
-            <StyledH1> Personality Quiz! </StyledH1>
-
-            <StyledLink href="/register">To Register </StyledLink>
-            <StyledLink href="/login">To Login </StyledLink>
-            <StyledLink href={"/quizlanding"}>To Quiz Landing</StyledLink>
-        </StyledContainer>
-    </LandingDiv>
-  );
+                    {/*<StyledLink href="/register">To Register </StyledLink>*/}
+                    <StyledButtonContainer>
+                        <StyledCustomLink href="/login">
+                            <StyledButton>To Login</StyledButton>
+                        </StyledCustomLink>
+                        {/*<StyledLink href={"/quizlanding"}>To Quiz Landing</StyledLink>*/}
+                        <StyledCustomLink href={"/quizlanding"}>
+                            <StyledButton>Start Quiz</StyledButton>
+                        </StyledCustomLink>
+                    </StyledButtonContainer>
+                </StyledContainer>
+            </LandingDiv>
+        </>
+    );
 }
+
