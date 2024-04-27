f6c47fe853492f4b951a9e8d901ed5f02f6066a7
Emma Kim <emmakim@crc-dot1x-nat-10-239-70-131.bu.edu>
2024-04-27 18:11:59 -0400

    quiz

--- src/app/(unauthenticated)/quiz/page.tsx
+++ src/app/(unauthenticated)/quiz/page.tsx
@@ -1,12 +1,25 @@
 import QuizQuestion from "@/component/quiz/quizquestion";
+import Starfield from "@/component/backgrounds/Starfield";
+import {LandingDiv, StyledContainer, StyledH1, StyledLink} from "@/Styles/GeneralStyles";
+import LoginForm from "@/component/login/LoginForm";
+import React from "react";
+
 
 export default function Quiz(){
     return (
         <>
-            <div>
-                <h1>Quiz</h1>
-                <QuizQuestion />
-            </div>
+            <LandingDiv>
+                <StyledContainer>
+                    <StyledH1> Quiz </StyledH1>
+                    <QuizQuestion/>
+                </StyledContainer>
+            </LandingDiv>
+            <Starfield
+                starCount={10000}
+                starColor={[255, 255, 255]}
+                speedFactor={0.03}
+                backgroundColor="black"
+            />
         </>
     );
 }
