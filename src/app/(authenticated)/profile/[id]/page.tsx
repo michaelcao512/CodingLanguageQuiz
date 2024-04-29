4cea71300e87ca6323a89b212e5a0b1edb98dfee
Matthew T. Iwane <mattiwane@gmail.com>
2024-04-29 19:33:45 -0400

    updated profile page

--- src/app/(authenticated)/profile/[id]/page.tsx
+++ src/app/(authenticated)/profile/[id]/page.tsx
@@ -1,31 +1,47 @@
 "use client";
 
-
 import { getPersonalityType, getUser } from "@/lib/database";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+import { StyledContainer, StyledP, StyledH1 } from "@/Styles/GeneralStyles";
+import { PersonalityType } from "@prisma/client";
+
 export default function UserProfile() {
   const params = useParams<{ id: string }>();
-  const [userInfo, setUserInfo] = useState<UserData>();
+
+  const [loading, setLoading] = useState(true);
+  const [userInfo, setUserInfo] = useState<{
+    id: number;
+    name: string;
+    email: string;
+    biography: string;
+    personalityTypeName: string;
+    personalityTypeDescription: string;
+  }>();
 
   useEffect(() => {
     async function getUserInfo() {
       try {
         const user = await getUser(parseInt(params.id));
-      console.log("USER", user);
-        const personalityType = await getPersonalityType(
+        console.log("USER", user);
+        const personalityType: PersonalityType = await getPersonalityType(
           user.personalityTypeId || -1
         );
         const personalityTypeName = personalityType ? personalityType.name : "";
+        const personalityTypeDescription = personalityType ? personalityType.description : "";
+
         const data = {
           id: user.id,
           name: user.name || "",
           email: user.email || "",
           biography: user.biography || "",
-          personalityType: personalityTypeName,
+          personalityTypeName: personalityTypeName,
+          personalityTypeDescription: personalityTypeDescription
+
         };
         setUserInfo(data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching users", error);
       }
@@ -34,11 +50,17 @@ export default function UserProfile() {
   }, [params.id]);
 
   return (
-    <>
-      <p>{userInfo?.name}</p>
-      <p>{userInfo?.biography}</p>
-      <p>{userInfo?.email}</p>
-      <p>{userInfo?.personalityType}</p>
-    </>
+    <StyledContainer>
+      {loading ? (<StyledH1>LOADING...</StyledH1>) : (
+        <>
+          <StyledH1>{userInfo?.name}</StyledH1>
+          <StyledP>{userInfo?.biography}</StyledP>
+          <StyledP>{userInfo?.email}</StyledP>
+          <StyledP>{userInfo?.personalityTypeName}</StyledP>
+          <StyledP>{userInfo?.personalityTypeDescription}</StyledP>
+
+        </>
+      )}
+    </StyledContainer>
   );
 }
