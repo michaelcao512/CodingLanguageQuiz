731d90628cee9fbf29b3068b119358a494963f16
Anh <anhtran8@bu.edu>
2024-04-29 13:42:01 -0400

    adding question card component

---
+++ src/component/quiz/QuestionCard.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const ChoiceCard = styled.div`
+    border: 1px solid #ddd;
+    padding: 16px;
+    margin: 8px 0;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    
+    &:hover {
+        background-color: white;
+        color: black;
+    }
+
+`;
+
+
+export default ChoiceCard;
