3b689106e51103e4e952e20edd14a3c85ef96fce
Michael Cao <mcao124@bu.edu>
2024-05-02 13:49:23 -0400

    prisma accelerate for connection pooling

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-let prisma: PrismaClient;
-declare const global: {
-    prisma: PrismaClient
-
-}
-
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-}
+const prisma = new PrismaClient().$extends(withAccelerate())
 
 export default prisma;
